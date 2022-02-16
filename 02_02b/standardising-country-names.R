ffca435384bd4ff3524b8e4f052d288e80d47440
charliejhadley <charliejhadley@gmail.com>
2022-02-16 15:25:34 +0000

    Update standardising-country-names.R

--- 02_02b/standardising-country-names.R
+++ 02_02b/standardising-country-names.R
@@ -25,7 +25,7 @@ country_population <- read_excel("data/country-gdp.xlsx")
 
 # ==== Data visualisation Code ====
 
-pal_gdp <- colorNumeric("viridis", world_population_sf$gdp_md_est)
+pal_gdp <- colorNumeric("viridis", world_gdp$gdp_md_est)
 
 world_gdp %>% 
   leaflet() %>% 
