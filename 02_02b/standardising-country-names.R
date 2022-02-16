698913aec889fdbb03578709857112cee2fe3474
charliejhadley <charliejhadley@gmail.com>
2022-02-16 15:18:34 +0000

    setup 02_02b

---
+++ 02_02b/standardising-country-names.R
@@ -0,0 +1,38 @@
+library(sf)
+library(readxl)
+library(leaflet)
+library(countrycode)
+library(tidyverse)
+
+# ==== Import ====
+
+world_sf <- read_sf("data/world-shapefiles")
+
+country_population <- read_excel("data/country-gdp.xlsx")
+
+# ==== Join ====
+
+
+
+
+
+# ==== Standardise country names ====
+
+
+
+
+
+
+# ==== Data visualisation Code ====
+
+pal_gdp <- colorNumeric("viridis", world_population_sf$gdp_md_est)
+
+world_population_sf %>% 
+  leaflet() %>% 
+  addPolygons(fillColor = ~pal_gdp(gdp_md_est),
+              color = "white",
+              fillOpacity = 1,
+              weight = 1) %>% 
+  addLegend(pal = pal_gdp,
+            values = ~gdp_md_est,
+            opacity = 1)
