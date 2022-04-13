f4a22e200b898ffeff5af0636b067bb5a0495ed5
charliejhadley <charliejhadley@gmail.com>
2022-04-13 17:21:00 +0100

    finished example

--- 03_03e/sf-and-tidyverse.R
+++ 03_03e/sf-and-tidyverse.R
@@ -1,6 +1,22 @@
 library(sf)
 library(tidyverse)
+library(mapview)
 
 us_states <- read_sf("data/us-states")
 
 state_population <- read_csv("data/state-population.csv")
+
+
+us_population_sf <- us_states %>% 
+  left_join(state_population,
+            by = c("NAME" = "state"))
+
+us_population_sf %>% 
+  filter(population < 1E6)
+
+us_region_pop <- us_population_sf %>% 
+  group_by(REGION) %>% 
+  summarise(region_population = sum(population))
+
+us_region_pop %>% 
+  mapview()
