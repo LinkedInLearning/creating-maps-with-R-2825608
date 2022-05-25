4712bb1501e8eddf04889c617d9ed21a7aef14ef
charliejhadley <charliejhadley@gmail.com>
2022-05-25 16:22:07 +0100

    04_01e

---
+++ 04_01e/geo-locations-with-ggplot2.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(sf)
+library(maps)
+library(rnaturalearthdata)
+
+brazil_sf <- countries110 %>% 
+  st_as_sf() %>% 
+  filter(name == "Brazil")
+
+brazil_cities <- world.cities %>% 
+  filter(country.etc == "Brazil",
+         pop >= 1E6) %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+  arrange(desc(pop))
+
+
+ggplot() +
+  geom_sf(data = brazil_sf,
+          fill = "darkolivegreen3") +
+  geom_sf(data = brazil_cities,
+          shape = 21,
+          aes(fill = as.logical(capital),
+              size = pop)) +
+  scale_size_area(max_size = 10) +
+  theme_void()
