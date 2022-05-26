8465d4e000fc1672682b4ffcff5bbe3423f01c99
charliejhadley <charliejhadley@gmail.com>
2022-05-26 13:28:50 +0100

    04_02e setup

---
+++ 04_02e/london-school-leavers.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(sf)
+
+london_sf <- read_sf("data/london_boroughs")
+
+education_data <- read_csv("data/age-when-completed-education.csv")
+
+london_school_leavers_sf <- london_sf %>% 
+  left_join(education_data,
+            by = c("lad11nm" = "area")) %>% 
+  filter(age_group == "16 or under")
+
+ggplot() +
+  geom_sf(data = london_school_leavers_sf,
+          aes(fill = value,
+              shape = "City of London"),
+          color = "white",
+          size = 0.2) +
+  scale_fill_viridis_c(labels = scales::number_format(big.mark = ","),
+                       name = "School leavers\n16 or under",
+                       na.value = "pink") +
+  guides(shape = guide_legend(override.aes = list(fill = "pink"),
+                              title = NULL)) +
+  labs(title = "Central London residents are less likely to leave school at 16") +
+  theme_void()
+
