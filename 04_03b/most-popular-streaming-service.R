951d49e24439745dfeda7716a5f35a814006e9da
charliejhadley <charliejhadley@gmail.com>
2022-05-26 15:30:02 +0100

    04_03b

---
+++ 04_03b/most-popular-streaming-service.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(sf)
+library(janitor)
+library(rmapshaper)
+library(tigris)
+
+most_popular_streaming_service <- read_csv("data/most-popular-streaming-service.csv") %>% 
+  clean_names()
+
+us_contiguous <- states() %>% 
+  clean_names() %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
+  ms_simplify()
+
+us_most_popular_streaming_service <- us_contiguous %>% 
+  left_join(most_popular_streaming_service,
+            by = c("name" = "state"))
+
+colors_services <- list(
+  "Amazon Prime" = "#2A96D9",
+  "ESPN" = "#cc5445",
+  "Hulu" = "#35B12E",
+  "Netflix" = "grey30"
+)
+
