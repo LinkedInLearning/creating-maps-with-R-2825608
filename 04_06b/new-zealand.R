90a40594f481f1eb12ee1caa10426184e197919d
charliejhadley <charliejhadley@gmail.com>
2022-05-31 16:25:09 +0100

    04_06b

---
+++ 04_06b/new-zealand.R
@@ -0,0 +1,24 @@
+library(maps)
+library(tidyverse)
+library(sf)
+library(rnaturalearthdata)
+
+new_zealand_sf <- countries50 %>% 
+  st_as_sf() %>% 
+  filter(name == "New Zealand")
+
+new_zealand_top_10_cities <- world.cities %>% 
+  filter(country.etc == "New Zealand") %>% 
+  slice_max(pop, n = 10) %>% 
+  arrange(desc(pop))
+
+new_zealand_top_10_cities_sf <- new_zealand_top_10_cities %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326)
+
+ggplot() +
+  geom_sf(data = new_zealand_sf) +
+  geom_sf(data = new_zealand_top_10_cities_sf,
+          aes(size = pop),
+          shape = 21,
+          alpha = 0.6,
+          fill = "purple")
