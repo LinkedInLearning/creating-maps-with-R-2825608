5b7df4538362644c1b102bba7442716c31bc77d2
charliejhadley <charliejhadley@gmail.com>
2022-06-01 13:09:31 +0100

    04_08b

---
+++ 04_08b/labelled-geobubble-chart.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(sf)
+library(maps)
+library(rnaturalearthdata)
+library(ggrepel)
+
+germany_sf <- countries50 %>% 
+  st_as_sf() %>% 
+  filter(name == "Germany")
+
+germany_cities <- world.cities %>% 
+  filter(country.etc == "Germany") %>% 
+  slice_max(pop, n = 5)
+
+
+ggplot() +
+  geom_sf(data = brazil_sf,
+          fill = "darkolivegreen3") +
+  theme_void()
