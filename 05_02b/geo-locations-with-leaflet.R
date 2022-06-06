1b80f3af23e0f4a3e0c0f71a4a25d970d5f8bba6
charliejhadley <charliejhadley@gmail.com>
2022-06-06 19:43:47 +0100

    05_02b

---
+++ 05_02b/geo-locations-with-leaflet.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(sf)
+library(maps)
+library(leaflet)
+library(rnaturalearthdata)
+
+brazil_sf <- countries110 %>% 
+  st_as_sf() %>% 
+  filter(name == "Brazil")
+
+brazil_cities <- world.cities %>% 
+  filter(country.etc == "Brazil",
+         pop >= 1E6) %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+  arrange(desc(pop))
+
+leaflet() %>% 
+  addPolygons(data = brazil_sf,
+              fillColor = "darkolivegreen",
+              fillOpacity = 1,
+              color = "black",
+              weight = 1
+              )
