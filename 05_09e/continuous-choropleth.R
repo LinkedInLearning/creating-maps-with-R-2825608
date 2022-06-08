a87589da5e020534c47c875eb34400168cab87cf
charliejhadley <charliejhadley@gmail.com>
2022-06-08 11:50:21 +0100

    05_09e

---
+++ 05_09e/continuous-choropleth.R
@@ -0,0 +1,39 @@
+library(tigris)
+library(sf)
+library(rmapshaper)
+library(tidyverse)
+library(janitor)
+library(tidycensus)
+library(leaflet)
+
+us_states <- states() %>% 
+  clean_names() %>% 
+  mutate(statefp = as.numeric(statefp))
+
+south_atlantic_states <- us_states %>% 
+  filter(division == 5) %>% 
+  ms_simplify()
+
+prisoners_per_state <- get_decennial(geography = "state",
+              variables = c("state_prison_population" = "PCT020006")) %>% 
+  clean_names() %>% 
+  mutate(state_prison_population = if_else(name == "District of Columbia",
+                         NA_real_,
+                         value))
+
+south_atlantic_prisons <- south_atlantic_states %>% 
+  left_join(prisoners_per_state)
+
+leaflet() %>% 
+  addPolygons(data = south_atlantic_prisons)
+
+
+
+
+
+# legend fix --------------------------------------------------------------
+
+html_fix <- htmltools::tags$style(type = "text/css", "div.info.legend.leaflet-control br {clear: both;}") 
+
+lf_map %>% 
+  htmlwidgets::prependContent(html_fix)
