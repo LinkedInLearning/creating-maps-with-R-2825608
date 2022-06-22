1a93b90ccc049c26688486723a40bbaf736f97ed
charliejhadley <charliejhadley@gmail.com>
2022-06-22 13:42:07 +0100

    switch to coastline maps

--- 06_02b/images-for-slides.R
+++ 06_02b/images-for-slides.R
@@ -15,22 +15,23 @@ theme_cjh_map <- function(){
 gg_web_mercator <- countries_sf %>%
   st_transform(crs = 3857) %>% 
   ggplot() +
-  geom_sf(fill = "#D2B48C") +
+  geom_sf(fill = "#D2B48C",
+          color = "transparent") +
   coord_sf(crs = 3857, expand = TRUE) +
-  theme_cjh_map()
-
-gg_web_mercator
-
+  theme_cjh_map() +
+  coord_sf(ylim = c(-pi * 6378137, pi * 6378137))
 
 gg_robinson <- countries_sf %>%
   ggplot() +
-  geom_sf(fill = "#D2B48C") +
+  geom_sf(fill = "#D2B48C",
+          color = "transparent") +
   coord_sf(crs = "+proj=robin") +
   theme_cjh_map()
 
 gg_pall_peters <- countries_sf %>%
   ggplot() +
-  geom_sf(fill = "#D2B48C") +
+  geom_sf(fill = "#D2B48C",
+          color = "transparent") +
   coord_sf(crs = "+proj=cea", expand = FALSE) +
   theme_cjh_map()
 
