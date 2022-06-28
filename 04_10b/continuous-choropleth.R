84c3c8026e25b677b93db9918c6212b455cd30f2
charliejhadley <charliejhadley@gmail.com>
2022-06-28 17:11:09 +0100

    switch to km of coastline

--- 04_10b/continuous-choropleth.R
+++ 04_10b/continuous-choropleth.R
@@ -3,29 +3,23 @@ library(sf)
 library(rmapshaper)
 library(tidyverse)
 library(janitor)
-library(tidycensus)
 
 us_states <- states() %>% 
   clean_names() %>% 
-  mutate(statefp = as.numeric(statefp))
-
-south_atlantic_states <- us_states %>% 
-  filter(division == 5) %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
   ms_simplify()
 
-prisoners_per_state <- get_decennial(geography = "state",
-              variables = c("state_prison_population" = "PCT020006")) %>% 
-  clean_names() %>% 
-  mutate(state_prison_population = if_else(name == "District of Columbia",
-                         NA_real_,
-                         value))
 
-south_atlantic_prisons <- south_atlantic_states %>% 
-  left_join(prisoners_per_state)
+state_coastline <- read_csv("data/km-of-coastline.csv")
 
+us_coastline <- us_states %>% 
+  left_join(state_coastline,
+            by = c("name" = "state"))
 
 
 ggplot() +
-  geom_sf(data = south_atlantic_prisons) +
+  geom_sf(data = us_coastline) +
   theme_void()
 
