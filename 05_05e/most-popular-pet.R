9ea89fbe9294847db77aa71a9d8b66870c375190
charliejhadley <charliejhadley@gmail.com>
2022-06-28 14:31:16 +0100

    switch dataset, streaming service -> pets

---
+++ 05_05e/most-popular-pet.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(sf)
+library(janitor)
+library(rmapshaper)
+library(tigris)
+library(leaflet)
+
+most_popular_pets <- read_csv("data/pet-searches-by-state.csv") %>% 
+  clean_names()
+
+us_contiguous <- states() %>% 
+  clean_names() %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
+  ms_simplify()
+
+us_most_popular_pets <- us_contiguous %>% 
+  left_join(most_popular_pets,
+            by = c("name" = "state"))
+
+colors_pets <-
+  c(
+    "Hamster" = "#AE76A6",
+    "Guinea pig" = "#92BFB1",
+    "Chinchilla" = "#694A38",
+    "Sugar glider" = "#A61C3C",
+    "Bearded dragon" = "#FFAD05"
+  )
+
+pal_pets <- colorFactor(colors_pets, us_most_popular_pets$pet)
+
+leaflet() %>% 
+  addPolygons(data = us_most_popular_pets,
+              label = ~name,
+              weight = 1,
+              color = "black",
+              fillColor = ~pal_pets(pet),
+              fillOpacity = 1
+  )
