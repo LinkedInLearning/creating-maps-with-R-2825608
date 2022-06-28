3baf6dadc63e62c2931f4d384869e8d08d37394b
charliejhadley <charliejhadley@gmail.com>
2022-06-28 17:19:04 +0100

    switch to km coastline

--- 04_10e/continuous-choropleth.R
+++ 04_10e/continuous-choropleth.R
@@ -3,37 +3,32 @@ library(sf)
 library(rmapshaper)
 library(tidyverse)
 library(janitor)
-library(tidycensus)
 
 us_states <- states() %>% 
   clean_names() %>% 
-  mutate(statefp = as.numeric(statefp))
-
-south_atlantic_states <- us_states %>% 
-  filter(division == 5) %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
   ms_simplify()
 
-prisoners_per_state <- get_decennial(geography = "state",
-              variables = c("state_prison_population" = "PCT020006")) %>% 
-  clean_names() %>% 
-  mutate(state_prison_population = if_else(name == "District of Columbia",
-                         NA_real_,
-                         value))
 
-south_atlantic_prisons <- south_atlantic_states %>% 
-  left_join(prisoners_per_state)
+state_coastline <- read_csv("data/km-of-coastline.csv")
 
+us_coastline <- us_states %>% 
+  left_join(state_coastline,
+            by = c("name" = "state"))
 
 
 ggplot() +
-  geom_sf(data = south_atlantic_prisons,
-          aes(fill = state_prison_population,
-              shape = "Washington DC"),
-          colour = "white",
+  geom_sf(data = us_coastline,
+          aes(fill = km_of_coastline,
+              shape = "Zero coastline"),
+          color = "white",
           size = 0.2) +
-  scale_fill_viridis_c(name = "State prison population",
+  scale_fill_viridis_c(name = "Coastline (km)",
                        labels = scales::number_format(big.mark = ","),
-                       na.value = "pink") +
+                       na.value = "pink"
+  ) +
   guides(shape = guide_legend(override.aes = list(fill = "pink"),
                               title = NULL)) +
   theme_void()
