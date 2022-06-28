269d143ce104934c9c6e1e9f4640bc6dd3bc8e50
charliejhadley <charliejhadley@gmail.com>
2022-06-28 17:47:15 +0100

    switch to km coastline

--- 05_10e/continuous-choropleth.R
+++ 05_10e/continuous-choropleth.R
@@ -3,62 +3,43 @@ library(sf)
 library(rmapshaper)
 library(tidyverse)
 library(janitor)
-library(tidycensus)
 library(leaflet)
 library(leaflet.extras)
 
 us_states <- states() %>% 
   clean_names() %>% 
-  mutate(statefp = as.numeric(statefp))
-
-south_atlantic_states <- us_states %>% 
-  filter(division == 5) %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
   ms_simplify()
 
-prisoners_per_state <- get_decennial(geography = "state",
-              variables = c("state_prison_population" = "PCT020006")) %>% 
-  clean_names() %>% 
-  mutate(state_prison_population = if_else(name == "District of Columbia",
-                         NA_real_,
-                         value))
 
-south_atlantic_prisons <- south_atlantic_states %>% 
-  left_join(prisoners_per_state)
+state_coastline <- read_csv("data/km-of-coastline.csv")
 
+us_coastline <- us_states %>% 
+  left_join(state_coastline,
+            by = c("name" = "state"))
 
-pal_prison_pop <- colorNumeric("viridis", south_atlantic_prisons$state_prison_population)
+pal_coastline <- colorNumeric("viridis", us_coastline$km_of_coastline, na.color = "pink")
 
-popup_state_prisons <- function(state, prison_population){
-  
-  paste(
-    "<b>State:</b>", state,
-    "<br>",
-    "<b>State prison population:</b>", scales::number(prison_population, big.mark = ",")
-  )
-  
-}
-
-lf_map <- leaflet() %>% 
-  addPolygons(data = south_atlantic_prisons,
-              color = "black",
+leaflet() %>% 
+  addPolygons(data = us_coastline,
               weight = 1,
-              fillColor = ~pal_prison_pop(state_prison_population),
-              fillOpacity = 1) %>% 
-  addLegend(data = south_atlantic_prisons,
-            pal = pal_prison_pop,
-            values = ~state_prison_population,
+              color = "white",
+              fillOpacity = 1,
+              fillColor = ~pal_coastline(km_of_coastline)) %>% 
+  addLegend(data = us_coastline,
+            pal = pal_coastline,
+            values = ~km_of_coastline,
             opacity = 1,
-            na.label = "Washington DC",
-            title = "State prison population") %>% 
+            na.label = "Zero coastline",
+            title = "Coastline (km)") %>% 
   setMapWidgetStyle(style = list(background = "white"))
 
+# legend placement fix ----------------------------------------------------
 
+css_fix <- "div.info.legend.leaflet-control br {clear: both;}" # CSS to correct spacing
+html_fix <- htmltools::tags$style(type = "text/css", css_fix) 
 
-
-# legend fix --------------------------------------------------------------
-
-html_fix <- htmltools::tags$style(type = "text/css", "div.info.legend.leaflet-control br {clear: both;}") 
-
-lf_map %>% 
+lf_coastline %>% 
   htmlwidgets::prependContent(html_fix)
-
