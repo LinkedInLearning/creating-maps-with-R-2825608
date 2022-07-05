492b69e7e7014e1d36737d5fd52c013ed9cd5155
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-05 12:49:17 +0100

    after recording

--- 03_03e/sf-and-tidyverse.R
+++ 03_03e/sf-and-tidyverse.R
@@ -6,17 +6,18 @@ us_states <- read_sf("data/us-states")
 
 state_population <- read_csv("data/state-population.csv")
 
-
-us_population_sf <- us_states %>% 
+us_state_pop <- us_states %>% 
   left_join(state_population,
             by = c("NAME" = "state"))
 
-us_population_sf %>% 
+us_state_pop %>% 
+  select(NAME, population) %>% 
   filter(population < 1E6)
 
-us_region_pop <- us_population_sf %>% 
+
+us_region_population <- us_state_pop %>% 
   group_by(REGION) %>% 
   summarise(region_population = sum(population))
 
-us_region_pop %>% 
-  mapview()
+us_region_population %>% 
+  mapview(zcol = "region_population")
