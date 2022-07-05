434db8f784c814d6979a04e338f0fd8599268eae
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-05 11:25:16 +0100

    end of recording state

--- 02_03e/geocoding.R
+++ 02_03e/geocoding.R
@@ -6,23 +6,19 @@ library(janitor)
 library(tidyverse)
 
 international_addresses <- read_excel("data/street-addresses.xlsx",
-                           sheet = "International Addresses") %>%
+                                      sheet = "International Addresses") %>%
   clean_names()
-
-geo("58 West St #33A, NY")
 
 geo("58 West St #33A, NY", method = "iq")
 
-
-international_addresses_geocoded <- international_addresses %>% 
+geocoded_addresses <- international_addresses %>% 
   geocode(street = street_address,
           city = city,
           postalcode = post_code,
           country = country,
           method = "iq")
 
-
-international_addresses_geocoded %>% 
+geocoded_addresses %>% 
   st_as_sf(coords = c("long", "lat"),
            crs = 4326) %>% 
   mapview()
