456ffeee3d43c0878faddf99190cfe8522da5596
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-05 16:27:37 +0100

    after record

--- 03_05e/continent-map.R
+++ 03_05e/continent-map.R
@@ -6,10 +6,14 @@ world_sf <- read_sf("data/world-shapefiles")
 
 country_population <- read_csv("data/country-population.csv")
 
-
+world_pop <- world_sf %>% 
+  left_join(country_population) %>% 
+  filter(name != "Antarctica") %>% 
+  group_by(continent) %>% 
+  summarise(continent_population = sum(pop_est, na.rm = TRUE))
 
 
 
 
 world_pop %>% 
-  mapview(zcol = "continent")
+  mapview(zcol = "continent_population")
