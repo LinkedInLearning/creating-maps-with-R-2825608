5f077589143677a96765242c8e70011a483608ac
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-05 16:14:54 +0100

    Update continent-map.R

--- 03_05e/continent-map.R
+++ 03_05e/continent-map.R
@@ -6,10 +6,10 @@ world_sf <- read_sf("data/world-shapefiles")
 
 country_population <- read_csv("data/country-population.csv")
 
-world_sf %>% 
-  mapview(zcol = "continent")
 
-world_sf %>% 
-  left_join(country_population) %>% 
-  filter(name != "Antarctica") %>% 
-  mapview(zcol = "pop_est")
+
+
+
+
+world_pop %>% 
+  mapview(zcol = "continent")
