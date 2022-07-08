1ccb980cb7a9e0f12e7c78f9e60712e70526bbd6
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-08 11:25:33 +0100

    after recording

--- 04_05e/coord-sf.R
+++ 04_05e/coord-sf.R
@@ -6,7 +6,7 @@ library(tigris)
 oregon_sf <- counties(state = "OR") %>% 
   clean_names()
 
-oregon_hospitals <- read_csv("oregon-hospital-locations.csv")
+oregon_hospitals <- read_csv("data/oregon-hospital-locations.csv")
 
 oregon_hospitals <- oregon_hospitals %>% 
   st_as_sf(coords = c("long", "lat"), crs = 4326)
@@ -21,19 +21,19 @@ ggplot() +
   labs(title = "Hospital locations in Oregon") +
   theme_void()
 
-multnomah_bbox <- oregon_sf %>% 
+
+bbox_multnomah <- oregon_sf %>% 
   filter(name == "Multnomah") %>% 
   st_bbox() %>% 
   as.list()
-
 
 ggplot() +
   geom_sf(data = oregon_sf,
           fill = "darkolivegreen3",
           color = "white") +
-  geom_sf(data = filter(oregon_hospitals, county == "Multnomah"),
+  geom_sf(data = oregon_hospitals,
           color = "darkblue") +
-  coord_sf(xlim = c(multnomah_bbox$xmin, multnomah_bbox$xmax),
-           ylim = c(multnomah_bbox$ymin, multnomah_bbox$ymax)) +
-  labs(title = "Hospital locations in Multnomah") +
+  labs(title = "Hospital locations in Oregon") +
+  coord_sf(xlim = c(bbox_multnomah$xmin, bbox_multnomah$xmax),
+           ylim = c(bbox_multnomah$ymin, bbox_multnomah$ymax)) +
   theme_void()
