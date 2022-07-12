d5e22c1511cd16bcde1ee8c8d1c2061d7ec6450d
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-12 14:49:27 +0100

    after recording

--- 05_10e/continuous-choropleth.R
+++ 05_10e/continuous-choropleth.R
@@ -20,20 +20,33 @@ us_coastline <- us_states %>%
   left_join(state_coastline,
             by = c("name" = "state"))
 
-pal_coastline <- colorNumeric("viridis", us_coastline$km_of_coastline, na.color = "pink")
 
-leaflet() %>% 
+pal_coastline_km <- colorNumeric("viridis", us_coastline$km_of_coastline,
+                                 na.color = "pink")
+
+
+popup_state_coastline <- function(state_name, coastline_km){
+  
+  paste(
+    "<b>State:</b>", state_name,
+    "<br>",
+    "<b>Coastline length:</b>", scales::number(coastline_km, suffix = "km", accuracy = 1)
+  )
+  
+}
+
+lf_coastline <- leaflet() %>% 
   addPolygons(data = us_coastline,
               weight = 1,
               color = "white",
+              fillColor = ~pal_coastline_km(km_of_coastline),
               fillOpacity = 1,
-              fillColor = ~pal_coastline(km_of_coastline)) %>% 
+              popup = ~popup_state_coastline(name, km_of_coastline)) %>% 
   addLegend(data = us_coastline,
-            pal = pal_coastline,
+            pal = pal_coastline_km,
             values = ~km_of_coastline,
             opacity = 1,
-            na.label = "Zero coastline",
-            title = "Coastline (km)") %>% 
+            na.label = "Zero coastline") %>% 
   setMapWidgetStyle(style = list(background = "white"))
 
 # legend placement fix ----------------------------------------------------
