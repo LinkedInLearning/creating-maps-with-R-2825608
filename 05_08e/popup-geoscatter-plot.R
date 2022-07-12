50b13e26b9c67cd9bab468173e3ca6c5478c6f17
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-12 13:19:06 +0100

    after record

--- 05_08e/popup-geoscatter-plot.R
+++ 05_08e/popup-geoscatter-plot.R
@@ -12,23 +12,21 @@ germany_sf <- countries50 %>%
 germany_cities <- world.cities %>% 
   filter(country.etc == "Germany") %>% 
   slice_max(pop, n = 5) %>% 
-  st_as_sf(coord = c("long", "lat"),
-           crs = 4326)
-
-popup_city_pop <- function(city, population){
-  
-  paste(
-    "<b>City:</b>", city,
-    "<br>",
-    "<b>Population</b>", scales::number(population, big.mark = ",")
-  )
-  
-}
-
-germany_cities <- germany_cities %>% 
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4326) %>% 
   mutate(city_type = ifelse(capital == 1, "Capital City", "City"))
 
 pal_city_type <- colorFactor(c("gold", "purple"), c("Capital City", "City"))
+
+popup_city_population <- function(city_name, population){
+  
+  paste(
+    "<b>City:</b>", city_name,
+    "<br>",
+    "<b>Poppulation:</b>", population
+  )
+  
+}
 
 leaflet() %>% 
   addPolygons(data = germany_sf,
@@ -38,12 +36,15 @@ leaflet() %>%
               fillOpacity = 1) %>% 
   addCircleMarkers(data = germany_cities,
                    weight = 1,
-                   color = "black",
                    fillColor = ~pal_city_type(city_type),
                    fillOpacity = 1,
-                   popup = ~popup_city_pop(name, pop)) %>% 
+                   popup = ~popup_city_population(name, pop)) %>% 
   addLegend(data = germany_cities,
             pal = pal_city_type,
             values = ~city_type,
             opacity = 1) %>% 
   setMapWidgetStyle(style = list(background = "white"))
+
+
+
+
