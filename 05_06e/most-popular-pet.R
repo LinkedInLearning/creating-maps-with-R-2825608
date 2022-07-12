abd8f7fe1450cc94ba2b74f66f5542b0b130b53c
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-12 12:47:45 +0100

    switch dataset

---
+++ 05_06e/most-popular-pet.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(sf)
+library(janitor)
+library(rmapshaper)
+library(tigris)
+library(leaflet)
+
+most_popular_pets <- read_csv("data/pet-searches-by-state.csv") %>% 
+  clean_names()
+
+
+order_popular_pets <- most_popular_pets %>% 
+  count(pet, sort = TRUE) %>% 
+  pull(pet)
+
+us_contiguous <- states() %>% 
+  clean_names() %>% 
+  mutate(statefp = as.numeric(statefp)) %>% 
+  filter(statefp < 60,
+         !statefp %in% c(2, 15)) %>% 
+  ms_simplify()
+
+us_most_popular_pets <- us_contiguous %>% 
+  left_join(most_popular_pets,
+            by = c("name" = "state")) %>% 
+  mutate(pet = fct_relevel(pet, order_popular_pets))
+
+colors_pets <-
+  c(
+    "Hamster" = "darkorchid1",
+    "Guinea pig" = "darkcyan",
+    "Chinchilla" = "chocolate",
+    "Sugar glider" = "darkred",
+    "Bearded dragon" = "gold"
+  )
+
+colors_pets <- colors_pets[order_popular_pets]
+
+pal_popular_pet <- colorFactor(colors_pets, us_most_popular_pets$pet,
+                               na.color = "darkblue")
+
+
+leaflet() %>% 
+  addPolygons(data = us_most_popular_pets,
+              label = ~name,
+              weight = 1,
+              fillColor = ~pal_popular_pet(pet),
+              fillOpacity = 1) %>% 
+  addLegend(data = us_most_popular_pets,
+            pal = pal_popular_pet,
+            values = ~pet,
+            opacity = 1,
+            na.label = "Data not recorded")
