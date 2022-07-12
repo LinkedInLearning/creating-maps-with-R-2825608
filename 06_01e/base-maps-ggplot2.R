9e137092d4667ef6b3220285354900dcde7a01a1
Charlie Joey Hadley <charlie@visibledata.co.uk>
2022-07-12 15:24:31 +0100

    after record

---
+++ 06_01e/base-maps-ggplot2.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(sf)
+library(maps)
+library(rnaturalearthdata)
+library(ggspatial)
+
+brazil_sf <- countries110 %>% 
+  st_as_sf() %>% 
+  filter(name == "Brazil")
+
+brazil_cities <- world.cities %>% 
+  filter(country.etc == "Brazil",
+         pop >= 1E6) %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+  arrange(desc(pop))
+
+
+ggplot() +
+  annotation_map_tile(zoom = 4) +
+  geom_sf(data = brazil_sf,
+          fill = "transparent") 
+
+ggplot() +
+  annotation_map_tile(zoom = 4) +
+  geom_sf(data = brazil_sf,
+          fill = "transparent") +
+  geom_sf(data = brazil_cities,
+          shape = 21,
+          aes(fill = as.logical(capital),
+              size = pop)) +
+  scale_size_area(max_size = 10,
+                  name = "City population",
+                  labels = scales::number_format(scale = 1E-6,
+                                                 suffix = " Million")) +
+  scale_fill_manual(name = "",
+                    values = c("TRUE" = "gold",
+                               "FALSE" = "purple"),
+                    labels = c("TRUE" = "Capital City",
+                               "FALSE" = "City")) +
+  guides(fill = guide_legend(override.aes = list(size = 5)),
+         size = guide_legend(override.aes = list(fill = "gray70"))) +
+  theme_void()
+
---
+++ 06_01e/base-maps-with-leaflet.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(sf)
+library(maps)
+library(leaflet)
+library(rnaturalearthdata)
+
+brazil_sf <- countries110 %>% 
+  st_as_sf() %>% 
+  filter(name == "Brazil")
+
+brazil_cities <- world.cities %>% 
+  filter(country.etc == "Brazil",
+         pop >= 1E6) %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+  arrange(desc(pop)) %>% 
+  mutate(city_type = ifelse(capital == 1, "Capital City", "City"))
+
+city_type_pal <- colorFactor(c("Gold", "Purple"), brazil_cities$city_type)
+
+leaflet() %>% 
+  addProviderTiles(providers$HERE.satelliteDay) %>% 
+  addPolygons(data = brazil_sf,
+              fillColor = "darkolivegreen",
+              fillOpacity = 0,
+              color = "black",
+              weight = 2,
+              opacity = 1
+  )
+
+
+leaflet() %>% 
+  addProviderTiles(providers$HERE.satelliteDay) %>% 
+  addPolygons(data = brazil_sf,
+              fillColor = "darkolivegreen",
+              fillOpacity = 0,
+              color = "black",
+              weight = 2,
+              opacity = 1
+  ) %>% 
+  addCircleMarkers(data = brazil_cities,
+                   weight = 1,
+                   color = "black",
+                   fillColor = ~city_type_pal(city_type),
+                   fillOpacity = 1,
+                   radius = ~scales::rescale(sqrt(pop), c(5, 20))) %>% 
+  addLegend(pal = city_type_pal,
+            data = brazil_cities,
+            values = ~city_type,
+            title = "")
+
+
